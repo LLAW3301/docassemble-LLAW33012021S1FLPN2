209782d0c9a4575cbcec4dfae9b3b24c9aba3ca3
Michael Doroch <80140523+doro0020@users.noreply.github.com>
2021-05-31 13:54:10 +0930

    Changed region selection to allow multiple regions, updated number --> region text function, made logic recognise multiple regions when choosing eligibility

--- docassemble/LLAW33012021S1FLPN2/FLPN_Functions.py
+++ docassemble/LLAW33012021S1FLPN2/FLPN_Functions.py
@@ -1,9 +1,22 @@
-def regionToString(region):
-  # if number is passed as string for whatever reason, cast to int
-  number = int(region)
-    
+def regionToString(regions):
   regionNumberMap = {1:"Adelaide Hills", 2:"Barossa Light and Lower North",3:"Eastern Adelaide",4:"Eyre Western",5:"Far North",6:"Fleurieu and Kangaroo Island",7:"Limestone Coast",8:"Murrary and Mallee",9:"Northern Adelaide",10:"Southern Adelaide",11:"Western Adelaide",12:"Yorke and Mid North"}
-  return regionNumberMap[number]
+  
+  
+  output = ""
+  
+  for num, trueFalse in regions.items():
+    if trueFalse == True:
+
+        # if number is passed as string for whatever reason, cast to int
+        number = int(num)
+        
+        if output == "":
+            output = "{}".format(regionNumberMap[number])
+        else:
+            output = "{}, {}".format(output, regionNumberMap[number]) 
+    
+  
+  return output
 
 # formats multi-string list into a single-line HTML string (to work around docassemble tables newline issue)
 def formatListIntoHTML(list):
